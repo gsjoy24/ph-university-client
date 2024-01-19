ffaffa59d059039ff276a6107b995627930a32aa
gsjoy24 <gour.joy24@gmail.com>
2024-01-19 19:06:46 +0600

    Add new admin paths

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -3,6 +3,34 @@ import CreateAdmin from '../pages/admin/CreateAdmin';
 import CreateFaculty from '../pages/admin/CreateFaculty';
 import CreateStudent from '../pages/admin/CreateStudent';
 
+const adminPaths2 = [
+	{
+		name: 'Dashboard',
+		path: '/admin/dashboard',
+		element: <AdminDashboard />
+	},
+	{
+		name: 'User Management',
+		children: [
+			{
+				name: 'Create Admin',
+				path: '/admin/create-admin',
+				element: <CreateAdmin />
+			},
+			{
+				name: 'Create Student',
+				path: '/admin/create-student',
+				element: <CreateStudent />
+			},
+			{
+				name: 'Create Faculty',
+				path: '/admin/create-faculty',
+				element: <CreateFaculty />
+			}
+		]
+	}
+];
+
 const adminPaths = [
 	{
 		path: 'dashboard',
