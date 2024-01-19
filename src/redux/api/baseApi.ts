2b40e56402ec77b23a061309347c5a82f27a0f1e
gsjoy24 <gour.joy24@gmail.com>
2024-01-20 03:11:30 +0600

    Refactor baseApi in src/redux/api/baseApi.ts

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,17 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseApi = createApi({
+export const baseApi = createApi({
 	reducerPath: 'baseApi',
 	baseQuery: fetchBaseQuery({
 		baseUrl: import.meta.env.VITE_SERVER_URL
 	}),
-	endpoints: (builder) => ({
-		login: builder.mutation({
-			query: (userInfo) => ({
-				url: '/auth/login',
-				method: 'POST',
-				body: userInfo
-			})
-		})
-	})
+	endpoints: () => ({})
 });
