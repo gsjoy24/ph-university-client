7649035b6d144b2aa91416344c254c5cfbf45096
gsjoy24 <gour.joy24@gmail.com>
2024-01-20 03:38:17 +0600

    Add login form and configure base API middleware

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,33 @@
+import { Button } from 'antd';
+import { useForm } from 'react-hook-form';
+import { useLoginMutation } from '../redux/features/auth/authApi';
+
 const Login = () => {
-	return <div>login</div>;
+	const { register, handleSubmit } = useForm({
+		defaultValues: {
+			id: 'A-0001',
+			password: 'securePassword123'
+		}
+	});
+	const [login, { data, error, isLoading }] = useLoginMutation();
+	const onSubmit = (data: any) => {
+		login(data);
+	};
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<div>
+				<label htmlFor='id'>ID:</label>
+				<input type='text' id='id' {...register('id')} />
+			</div>
+			<div>
+				<label htmlFor='password'>Password:</label>
+				<input type='text' id='password' {...register('password')} />
+			</div>
+			<Button type='text' htmlType='submit'>
+				Login
+			</Button>
+		</form>
+	);
 };
 
 export default Login;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/authSlice';
+import { baseApi } from './api/baseApi';
 
 export const store = configureStore({
 	reducer: {
+		[baseApi.reducerPath]: baseApi.reducer,
 		auth: authReducer
-	}
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware)
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
