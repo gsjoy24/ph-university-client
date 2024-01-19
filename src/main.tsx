b1112ea4148b1a4de5fe46c50fd5b0e204375164
gsjoy24 <gour.joy24@gmail.com>
2024-01-19 23:02:26 +0600

    Add Redux and React Redux dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import App from './App';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/routes';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
 	</React.StrictMode>
 );
