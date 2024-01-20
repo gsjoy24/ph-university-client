cb54d85623c94a8f816ddaa268f76458c29369f7
gsjoy24 <gour.joy24@gmail.com>
2024-01-20 19:47:07 +0600

    Add sonner package and navigate to dashboard after successful login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,7 @@ import { useLoginMutation } from '../redux/features/auth/authApi';
 import { useAppDispatch } from '../redux/hooks';
 import { setUser } from '../redux/features/auth/authSlice';
 import { verifyToken } from '../utils/verifyToken';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 	const { register, handleSubmit } = useForm({
@@ -13,12 +14,15 @@ const Login = () => {
 		}
 	});
 	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
 	const [login, { data, error, isLoading }] = useLoginMutation();
 
 	const onSubmit = async (data: any) => {
 		const res = await login(data).unwrap();
 		const userInfo = verifyToken(res.data.accessToken);
 		dispatch(setUser({ user: userInfo, token: res.data.accessToken }));
+
+		navigate(`/${userInfo.role}/dashboard`);
 	};
 	return (
 		<form onSubmit={handleSubmit(onSubmit)}>
