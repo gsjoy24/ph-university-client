64285c7b59a15838b460f8f2db6dd3294bb65237
gsjoy24 <gour.joy24@gmail.com>
2024-01-20 20:57:18 +0600

    Add useGetAllSemestersQuery hook to AcademicSemester component

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,4 +1,8 @@
+import { useGetAllSemestersQuery } from '../../../redux/features/academicSemester/academicSemesterApi';
+
 const AcademicSemester = () => {
+	const { data } = useGetAllSemestersQuery(null);
+	console.log(data);
 	return (
 		<div>
 			<h1>This is AcademicSemester component</h1>
