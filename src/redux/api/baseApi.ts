c14496a9b342c96c30ecbc04240fe96336bae4fe
gsjoy24 <gour.joy24@gmail.com>
2024-01-20 22:52:39 +0600

    Update baseApi to include authorization token in headers

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from '../store';
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: import.meta.env.VITE_SERVER_URL,
+	credentials: 'include',
+	prepareHeaders: (headers, { getState }) => {
+		const token = (getState() as RootState).auth.token;
+		if (token) {
+			headers.set('authorization', `${token}`);
+		}
+		return headers;
+	}
+});
+
+const baseQueryWithRefreshToken = async (args: any, api: any, extraOptions: any) => {
+	return await baseQuery(args, api, extraOptions);
+};
 
 export const baseApi = createApi({
 	reducerPath: 'baseApi',
-	baseQuery: fetchBaseQuery({
-		baseUrl: import.meta.env.VITE_SERVER_URL,
-		credentials: 'include'
-	}),
+	baseQuery: baseQueryWithRefreshToken,
 	endpoints: () => ({})
 });
