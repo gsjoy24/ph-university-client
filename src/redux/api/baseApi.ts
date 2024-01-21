0cf158470834ffa63b41a14bfdeb75219f856973
gsjoy24 <gour.joy24@gmail.com>
2024-01-21 20:29:24 +0600

    Add authentication token refresh logic to baseApi

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../store';
+import { logOut, setUser } from '../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
 	baseUrl: import.meta.env.VITE_SERVER_URL,
@@ -15,13 +16,27 @@ const baseQuery = fetchBaseQuery({
 });
 
 const baseQueryWithRefreshToken = async (args: any, api: any, extraOptions: any) => {
-	const result = await baseQuery(args, api, extraOptions);
+	let result = await baseQuery(args, api, extraOptions);
 	if (result.error?.status === 401) {
 		const refreshResult = await fetch(`auth/refresh-token`, {
 			method: 'POST',
 			credentials: 'include'
 		});
+		const data = await refreshResult.json();
+		if (data.data.accessToken) {
+			const user = api.getState().auth.user;
+			api.dispatch(
+				setUser({
+					user,
+					token: data.data.token
+				})
+			);
+			result = await baseQuery(args, api, extraOptions);
+		} else {
+			api.dispatch(logOut());
+		}
 	}
+	return result;
 };
 
 export const baseApi = createApi({
