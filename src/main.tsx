8f0b7fa16d46ef2f5ec9e16ae9d41cd0b0bcddc2
gsjoy24 <gour.joy24@gmail.com>
2024-01-30 02:24:31 +0600

    Remove unused import statement

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Toaster } from 'sonner';
-import App from './App';
 import './index.css';
 import { persistor, store } from './redux/store';
 import router from './routes/routes';
