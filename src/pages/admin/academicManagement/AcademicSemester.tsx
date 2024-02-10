f2e9c18ec6527722366ce59b2f375a3832bcc1f6
gsjoy24 <gour.joy24@gmail.com>
2024-02-11 02:07:00 +0600

    Update import path for academicSemesterApi

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,4 +1,4 @@
-import { useGetAllSemestersQuery } from '../../../redux/features/academicSemester/academicSemesterApi';
+import { useGetAllSemestersQuery } from '../../../redux/features/admin/academicManagement.api';
 
 const AcademicSemester = () => {
 	const { data } = useGetAllSemestersQuery(null);
--- src/redux/features/admin/academicManagement.api.ts
+++ src/redux/features/admin/academicManagement.api.ts
@@ -1,6 +1,6 @@
 import { baseApi } from '../../api/baseApi';
 
-const academicSemesterApi = baseApi.injectEndpoints({
+const academicManagementApi = baseApi.injectEndpoints({
 	endpoints: (builder) => ({
 		getAllSemesters: builder.query({
 			query: () => ({
@@ -11,4 +11,4 @@ const academicSemesterApi = baseApi.injectEndpoints({
 	})
 });
 
-export const { useGetAllSemestersQuery } = academicSemesterApi;
+export const { useGetAllSemestersQuery } = academicManagementApi;
