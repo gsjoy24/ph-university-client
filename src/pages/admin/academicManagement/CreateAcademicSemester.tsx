1a6f406a219b04ee73ca87e007fa53296420cbe9
gsjoy24 <gour.joy24@gmail.com>
2024-02-11 01:08:36 +0600

    Add form submission handling and console log semester data

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -3,6 +3,15 @@ import { FieldValues, SubmitHandler } from 'react-hook-form';
 import PHForm from '../../../components/form/PHForm';
 import PHSelect from '../../../components/form/PHSelect';
 
+type TSemesterData = {
+	code: string;
+	name: string;
+	year: string;
+	startMonth: string;
+	endMonth: string;
+};
+
+// for semester options
 const nameOptions = [
 	{
 		value: '01',
@@ -17,24 +26,34 @@ const nameOptions = [
 		label: 'Fall'
 	}
 ];
+// fr year options
+const currentYear = new Date().getFullYear();
+const yearOptions = Array.from({ length: 5 }, (_, i) => currentYear + i).map((year) => ({
+	value: year.toString(),
+	label: year.toString()
+}));
 
 const CreateAcademicSemester = () => {
-	const onSubmit: SubmitHandler<FieldValues> = (data: any) => {
-		const findData = nameOptions.find((item) => item.value === data.semester);
+	const onSubmit: SubmitHandler<FieldValues> = ({ code, year, startMonth, endMonth }: any) => {
+		const findData = nameOptions.find((item) => item.value === code);
 
-		const semesterData = {
-			code: data.semester,
-			name: findData?.label
+		const semesterData: TSemesterData = {
+			code,
+			name: findData!.label,
+			year,
+			startMonth,
+			endMonth
 		};
+
+		console.log(semesterData);
 	};
 
 	return (
 		<Flex justify='center' align='center'>
 			<Col span={8}>
 				<PHForm onSubmit={onSubmit}>
-					<PHSelect label='Semester' options={nameOptions} name='semester' />
-					<PHSelect label='Year' options={nameOptions} name='year' />
-					<PHSelect label='Year' options={nameOptions} name='year' />
+					<PHSelect label='Semester' options={nameOptions} name='code' />
+					<PHSelect label='Year' options={yearOptions} name='year' />
 					<PHSelect label='Start Month' options={nameOptions} name='startMonth' />
 					<PHSelect label='End Month' options={nameOptions} name='endMonth' />
 					<Button type='primary' htmlType='submit'>
