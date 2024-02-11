8b3b4973f2c81a27e75547e9105dfbd69f306e41
gsjoy24 <gour.joy24@gmail.com>
2024-02-12 00:10:45 +0600

    Refactor AcademicSemester component

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,68 +1,29 @@
 import { Table, TableColumnsType, TableProps } from 'antd';
 import { useGetAllSemestersQuery } from '../../../redux/features/admin/academicManagement.api';
+interface DataType {
+	key: React.Key;
+	name: string;
+	age: number;
+	address: string;
+}
 
 const AcademicSemester = () => {
-	interface DataType {
-		key: React.Key;
-		name: string;
-		age: number;
-		address: string;
-	}
-
 	const columns: TableColumnsType<DataType> = [
 		{
 			title: 'Name',
-			dataIndex: 'name',
-			filters: [
-				{
-					text: 'Joe',
-					value: 'Joe'
-				},
-				{
-					text: 'Jim',
-					value: 'Jim'
-				},
-				{
-					text: 'Submenu',
-					value: 'Submenu',
-					children: [
-						{
-							text: 'Green',
-							value: 'Green'
-						},
-						{
-							text: 'Black',
-							value: 'Black'
-						}
-					]
-				}
-			],
-			// specify the condition of filtering result
-			// here is that finding the name started with `value`
-			onFilter: (value: string, record) => record.name.indexOf(value) === 0,
-			sorter: (a, b) => a.name.length - b.name.length,
-			sortDirections: ['descend']
+			dataIndex: 'name'
 		},
 		{
-			title: 'Age',
-			dataIndex: 'age',
-			defaultSortOrder: 'descend',
-			sorter: (a, b) => a.age - b.age
+			title: 'Year',
+			dataIndex: 'year'
 		},
 		{
-			title: 'Address',
-			dataIndex: 'address',
-			filters: [
-				{
-					text: 'London',
-					value: 'London'
-				},
-				{
-					text: 'New York',
-					value: 'New York'
-				}
-			],
-			onFilter: (value: string, record) => record.address.indexOf(value) === 0
+			title: 'Start Month',
+			dataIndex: 'startMonth'
+		},
+		{
+			title: 'End Month',
+			dataIndex: 'endMonth'
 		}
 	];
 
@@ -97,8 +58,20 @@ const AcademicSemester = () => {
 		console.log('params', pagination, filters, sorter, extra);
 	};
 	const { data: allSemesters } = useGetAllSemestersQuery(null);
+
+	const tableData = allSemesters?.data.map(({ _id, name, startMonth, endMonth, year }) => {
+		return {
+			key: _id,
+			name,
+			startMonth,
+			endMonth,
+			year
+		};
+	});
+
+	const allSemestersData = allSemesters?.data;
 	console.log(allSemesters);
-	return <Table columns={columns} dataSource={data} onChange={onChange} />;
+	return <Table columns={columns} dataSource={tableData} onChange={onChange} />;
 };
 
 export default AcademicSemester;
